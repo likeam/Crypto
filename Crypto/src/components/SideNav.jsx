d3284cacd1b867689b5f2d40d4a04c2aec20cabe
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-04 21:04:47 +0500

    first commit

---
+++ Crypto/src/components/SideNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Box} from '@chakra-ui/react';
+
+const SideNav = () => {
+  return (
+    <Box>SideNav</Box>
+  )
+}
+
+export default SideNav
---
+++ Crypto/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideNav from '../../components/SideNav'
+
+const Dashboard = () => {
+  return (
+    <SideNav />
+  )
+}
+
+export default Dashboard
